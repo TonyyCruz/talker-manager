404c7787bf8836c82f57e88ba96153ba9bc8beed
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-08-02 17:39:07 -0300

    fix: requisito 1. Adiciona filtro para caso o arquivo lido esteja vazio

--- middleware.js
+++ middleware.js
@@ -1,8 +1,14 @@
 const fs = require('fs/promises');
 
-const getTalkers = async (req, res, next) => {
+// const catchJsonFile = (file) => JSON.parse(fs.readFile(file, 'utf-8'));
+
+const getTalkers = async (_req, res, next) => {
   try {
     const talkers = await fs.readFile('talker.json', 'utf-8');
+    if (talkers.length === 0 || talkers === '') {
+      return res.status(200).json([]);
+    }
+
     const jsonTalkers = JSON.parse(talkers);
     res.status(200).json(jsonTalkers);
   } catch (err) {
@@ -10,6 +16,21 @@ const getTalkers = async (req, res, next) => {
   }
 };
 
+// const getTalkerId = async (req, res, next) => {
+//   const { id } = req.params;
+
+//   try {
+//     const talkers = await fs.readFile('talker.json', 'utf-8');
+//     const jsonTalkers = JSON.parse(talkers);
+//     if (jsonTalkers === undefined) 
+
+//     res.status(200).json(jsonTalkers);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
 module.exports = {
   getTalkers,
+  // getTalkerId,
 };

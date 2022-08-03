45fab7acd476e7f33e01c38fd42ba1d555985046
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-08-02 21:37:07 -0300

    feat: requisito 4. adiciona os filtros de email e password

--- middlewares/loginMiddleware.js
+++ middlewares/loginMiddleware.js
@@ -1,3 +1,8 @@
+const emailIsRequired = 'O campo "email" é obrigatório';
+const invalidEmailFormat = 'O campo "email" deve ter o formato "email@email.com"';
+const passwordIsRequired = 'O campo "password" é obrigatório';
+const invalidPassLength = 'O "password" deve ter pelo menos 6 caracteres';
+
 const randomToken = () => {
   const token = Math.random().toString(36).substr(2);
   const newToken = token + token;
@@ -13,19 +18,11 @@ const validateEmail = (email) => {
 const loginVerify = async (req, res) => {
   const { email, password } = req.body;
 
-  if (email === undefined) {
-    return res.status(400).json({ message: 'o campo "email" é obrigatório' });
-  }
-
+  if (email === undefined) return res.status(400).json({ message: emailIsRequired });
   const emailCheck = validateEmail(email);
-  if (!emailCheck) {
-    return res.status(400).json({
-      message: 'o campo "email" deve ter o formato "email@email.com"' });
-  }
-
-  if (password === undefined) {
-    return res.status().json({ message: 'o campo "password" é obrigatório' });
-  }
+  if (!emailCheck) return res.status(400).json({ message: invalidEmailFormat });
+  if (password === undefined) return res.status(400).json({ message: passwordIsRequired });
+  if (password.length < 6) return res.status(400).json({ message: invalidPassLength });
 
   const token = randomToken();
   res.status(200).json({ token });
